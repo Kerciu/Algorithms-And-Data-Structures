e362b934e44f27bbea56e56653903f81dde99e6c
Kacper Gorski <01187228@pw.edu.pl>
2024-03-14 15:54:23 +0100

    initialized insertion sort algorithm

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,2 +1,9 @@
 def insertion_sort(arr):
-    pass
+    for i in range(len(arr)):
+
+        j = i
+        while j > 0 and arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
+
+    return arr
