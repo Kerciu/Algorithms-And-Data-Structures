44337697f2f86fb809e4cf21ae598baab5d59725
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-03-14 21:04:19 +0100

    finished_selection_sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,13 +1,8 @@
-def selection_sort(arr):
-
-    for i in range(len(arr)):
-        minimumIndex = i
-
-        for j in range(i + 1, len(arr)):
-
-            if arr[j] < arr[minimumIndex]:
-                minimumIndex = j
-    
-        (arr[i], arr[minimumIndex]) = (arr[minimumIndex], arr[i])
-
-    return arr
+def selection_sort(list):
+    for i in range(len(list)):
+        # capture current character
+        for j in range(i + 1, len(list)):
+            # iterate through the list above the captured character to the end
+            if list[j] < list[i]:
+                list[i], list[j] = list[j], list[i]
+    return list
