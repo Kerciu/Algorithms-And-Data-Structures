d5f268b5fa90606726d4a7c92a170c70d163e095
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-03-15 13:35:49 +0100

    refactored_length_calculating

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,9 @@
 def merge_sort(list):
-    if len(list) <= 1:
+    list_length = len(list)
+    if list_length <= 1:
         return list
     # count the middle element of the list,round down
-    middle_point = len(list) // 2
+    middle_point = list_length // 2
     # seperate list to two halfs
     left_part = list[:middle_point]
     right_part = list[middle_point:]
@@ -15,11 +16,13 @@ def merge_sort(list):
 
 def merge_lists(left_part, right_part):
     # point next unmerged element from each list
+    left_part_length = len(left_part)
+    right_part_length = len(right_part)
     left_index = 0
     right_index = 0
     merged_list = []
     # add next elements from lists to merged_list in correct order
-    while (left_index < len(left_part) and right_index < len(right_part)):
+    while (left_index < left_part_length and right_index < right_part_length):
         if left_part[left_index] > right_part[right_index]:
             merged_list.append(right_part[right_index])
             right_index += 1
@@ -27,8 +30,8 @@ def merge_lists(left_part, right_part):
             merged_list.append(left_part[left_index])
             left_index += 1
     # add rest of elements which were not added in the loop
-    if left_index < len(left_part):
+    if left_index < left_part_length:
         merged_list.extend(left_part[left_index:])
-    elif right_index < len(right_part):
+    elif right_index < right_part_length:
         merged_list.extend(right_part[right_index:])
     return merged_list
