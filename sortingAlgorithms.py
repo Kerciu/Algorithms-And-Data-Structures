594f86656ac45e747a5a8067dee53d3ae4b572b1
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-03-15 16:09:09 +0100

    added_reading_from_file

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -2,9 +2,26 @@ import bubble_sort, insertion_sort, merge_sort, quick_sort, selection_sort
 import sys
 
 
+PATH = "pan-tadeusz-unix.txt"
+
+
 def main():
     sys.setrecursionlimit(1000)
     pass
+
+
+def read_from_file(path):
+    with open(path) as file_handle:
+        list_of_words = []
+        for line in file_handle:
+            line.rstrip()
+            line.rstrip("\n")
+            line = line.split(" ")
+            forbidden_signs = ["", "-", "\n", "-----\n"]
+            for word in line:
+                if word not in forbidden_signs:
+                    list_of_words.append(word)
+    return list_of_words
 
 
 if __name__ == "__main__":
