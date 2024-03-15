bd891c0f2ca9d7993d0c2e36fe2dc07fe58d156f
kgorski1 <01187228@pw.edu.pl>
2024-03-15 11:47:56 +0100

    made plotter function, fixed linter problems

--- drawPlots.py
+++ drawPlots.py
@@ -4,21 +4,32 @@ import gc
 
 
 # Use this as proccess_algorithm_time()[0]
-def process_algorithm_time(function):
+def process_algorithm_time(sorting_algorithm, arr):
     # Disable garbage collector
     gc_old = gc.isenabled()
     gc.disable()
 
     # Compute time complexity
     start = time.process_time()
-    result = function()
+    sorting_algorithm(arr)
     stop = time.process_time()
 
     if gc_old:
         gc.enable()
 
-    return stop - start, result
+    return stop - start
 
 
-def draw_computional_complexity(data):
-    pass
+#TODO Create function that extracts data from results and
+# saves it to the dictionary.
+def draw_computional_complexity(size, sorting_time, info):
+    # x axis: elements
+    # y axis: computing time
+    plt.plot(size, sorting_time, label=f"{info}")
+    plt.xlabel("Size of Sorted Array")
+    plt.ylabel("Time Needed to Compute")
+
+    plt.title("Comparition Time Between Sorting Algorithms")
+    plt.legend()
+
+    plt.show()
