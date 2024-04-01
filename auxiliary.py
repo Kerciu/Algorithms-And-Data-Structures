5e72a5dd528656027f4e241fbdeb2617824041f0
Kerciu <kerciuuu@gmail.com>
2024-04-01 14:02:17 +0200

    added optional arguments to facilitate usage of plotter

--- auxiliary.py
+++ auxiliary.py
@@ -1,17 +1,34 @@
 from typing import List
 from matplotlib import pyplot as plt
 import random
+import time
+import gc
 
 
 def generate_random_list() -> List[int]:
     # generate array of random elements
-    unheapified_array = []
+    unheapified_array: List[int] = []
     for i in range(1, 1000000):
         unheapified_array.append(random.randint(1, 3000000))
     return unheapified_array
 
 
-def plotter(x_value, y_value, process):
+def process_algorithm_time(process, *args, **kwargs) -> float:
+    gc_old: bool = gc.isenabled()
+    gc.disable()
+
+    # Compute time complexity
+    start: float = time.process_time()
+    process(*args, **kwargs)
+    stop: float = time.process_time()
+
+    if gc_old:
+        gc.enable()
+
+    return stop - start
+
+
+def plotter(x_value: List[int], y_value: List[float], process: str) -> None:
     plt.plot(x_value, y_value, label=f"{process.capitalize()}")
     plt.xlabel(f"Integers to {process}")
     plt.ylabel("Time to compute")
--- mainModule.py
+++ mainModule.py
@@ -1,42 +1,40 @@
-from typing import List
+from typing import List, Tuple
 from Heap import Heap
-from auxiliary import generate_random_list, plotter
+from auxiliary import generate_random_list, plotter, process_algorithm_time
 from nodes import Node
-import gc
-import time
 
 
-def extract_info(arity: int) -> List[List[int]]:
-    heap = Heap(arity)
-
-    array = []
-    time_values = []
-    ranges = [10000 * i for i in range(1, 11)]      # 10 ranges
+def generate_ranges() -> Tuple[List[List[int]], List[int]]:
+    array: List[List[int]] = []
+    ranges: List[int] = [10000 * i for i in range(1, 11)]
     for value in ranges:
         array.append(generate_random_list()[:value])
+
+    return array, ranges
+
+
+def extract_info(process, array, ranges, *args, **kwargs) -> List[List[int]]:
+
+    time_values: List[int] = []
+
     for arr in array:
         # Disable garbage collector
-        gc_old = gc.isenabled()
-        gc.disable()
-
-        # Compute time complexity
-        start = time.process_time()
-        heap.heapify(arr, 0)
-        stop = time.process_time()
-
-        if gc_old:
-            gc.enable()
-
-        time_value = stop - start
-
+        time_value = process_algorithm_time(process, arr, *args, **kwargs)
         time_values.append(time_value)
 
     return [ranges, time_values]
 
 
-def main():
-    info = extract_info(2)
-    plotter(info[0], info[1], "heapify")
+def main() -> None:
+    heap = Heap(2)
+    array, ranges = generate_ranges()
+
+    heapify_info = extract_info(lambda x: heap.heapify(x, 0), array, ranges)
+    plotter(heapify_info[0], heapify_info[1], "heapify")
+    delete_info = extract_info(heap.delete_max, array, ranges)
+    plotter(delete_info[0], delete_info[1], "delete max key")
+    add_info = extract_info(lambda x: heap.add_max(x, 21370), array, ranges)
+    plotter(add_info[0], add_info[1], "add max key")
 
 
 if __name__ == "__main__":
