639480ae8263995b4bc957de4c8f6c14d1bae50f
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-04-12 18:26:25 +0200

    added_bst_tree

--- BSTTree.py
+++ BSTTree.py
@@ -10,6 +10,89 @@
 #        / \
 #      4.5  6
 #
-#  Difference in height between left and right tree in AVL tree must be equal to 1
+#  Difference in height between left and right tree
+#  in AVL tree must be equal to 1
 #  New element will always be a "leaf"
 #
+
+from BSTNode import BSTNode
+
+
+class BSTTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = BSTNode(value)
+        else:
+            self.insert_recursion(value, self.root)
+
+    def insert_recursion(self, value, root):
+        if value < root.value:
+            if not root.left:
+                root.left = BSTNode(value)
+            else:
+                self.insert_recursion(value, root.left)
+        elif value > root.value:
+            if not root.right:
+                root.right = BSTNode(value)
+            else:
+                self.insert_recursion(value, root.right)
+        else:
+            print("Number exists in the tree")
+
+    def search(self, value):
+        if not self.root:
+            return False
+        else:
+            return self.search_recursion(value, self.root)
+
+    def search_recursion(self, value, root):
+        if value < root.value:
+            if not root.left:
+                return False
+            else:
+                return self.search_recursion(value, root.left)
+        if value > root.value:
+            if not root.right:
+                return False
+            else:
+                return self.search_recursion(value, root.right)
+        return True
+
+    def delete(self, value):
+        self.root = self.delete_recursion(value, self.root)
+
+    def delete_recursion(self, value, root):
+        if root is None:
+            return root
+
+        if value < root.value:
+            root.left = self.delete_recursion(value, root.left)
+        elif value > root.value:
+            root.right = self.delete_recursion(value, root.right)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            # Node has two children, find successor and replace value
+            successor = self.find_successor(root.right)
+            root.value = successor.value
+            root.right = self.delete_recursion(successor.value, root.right)
+
+        return root
+
+    def find_successor(self, node):
+        current_node = node
+        while current_node.left:
+            current_node = current_node.left
+        return current_node
+
+    def find_predecessor(self, node):
+        current_node = node
+        while current_node.right:
+            current_node = current_node.right
+        return current_node
--- test_Trees.py
+++ test_Trees.py
@@ -1,60 +1,60 @@
-from BSTNode import BSTNode
-import random
+from BSTTree import BSTTree
 
 
 def test_create_tree():
-    tree = BSTNode(5)
-    values = [3, 8, 2, 4, 7, 10]
+    tree = BSTTree()
+    values = [5, 3, 8, 2, 4, 7, 10]
     for val in values:
         tree.insert(val)
 
-    assert tree.value == 5
-    assert tree.left.value == 3
-    assert tree.right.value == 8
-    assert tree.left.left.value == 2
-    assert tree.left.right.value == 4
-    assert tree.right.left.value == 7
-    assert tree.right.right.value == 10
+    assert tree.root.value == 5
+    assert tree.root.left.value == 3
+    assert tree.root.right.value == 8
+    assert tree.root.left.left.value == 2
+    assert tree.root.left.right.value == 4
+    assert tree.root.right.left.value == 7
+    assert tree.root.right.right.value == 10
 
 
 def test_search_for_element():
-    tree = BSTNode(10)
-    vals = [i for i in range(1, 10)]
-    random.shuffle([vals])
-    for val in vals:
+    tree = BSTTree()
+    tree_empty = BSTTree()
+    values = [i for i in range(1, 11)]
+    for val in values:
         tree.insert(val)
-
     assert tree.search(5) is True
     assert tree.search(11) is False
+    assert tree_empty.search(200) is False
 
 
 def test_find_successor():
-    tree = BSTNode(5)
-    values = [3, 8, 2, 4, 7, 10]
+    tree = BSTTree()
+    values = [5, 3, 8, 2, 4, 7, 10]
     for val in values:
         tree.insert(val)
-    assert tree.successor(tree.root) == 7
+
+    assert tree.find_successor(tree.root).value == 2
 
 
 def test_find_predecessor():
     tree = BSTTree()
     values = [5, 3, 8, 2, 4, 7, 10]
     for val in values:
-        tree.insertElement(BSTNode(val))
+        tree.insert(val)
 
-    assert tree.predecessor(tree.root) == 4
+    assert tree.find_predecessor(tree.root).value == 10
 
 
 def test_delete_element():
     tree = BSTTree()
     values = [5, 3, 8, 2, 4, 7, 10]
     for val in values:
-        tree.insertElement(BSTNode(val))
+        tree.insert(val)
 
-    tree.deleteElement(3)
-    tree.deleteElement(8)
-    tree.deleteElement(5)
+    tree.delete(3)
+    tree.delete(8)
+    tree.delete(5)
 
-    assert tree.searchForElement(3) is False
-    assert tree.searchForElement(8) is False
-    assert tree.searchForElement(5) is False
+    assert tree.search(3) is False
+    assert tree.search(8) is False
+    assert tree.search(5) is False
