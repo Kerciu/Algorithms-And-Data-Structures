a5777ecc7b038fb6a9e78703ff0443e7c5e8c131
kgorski1 <01187228@pw.edu.pl>
2024-04-15 21:32:01 +0200

    Added user-friendly terminal communicates

--- mainModule.py
+++ mainModule.py
@@ -9,41 +9,55 @@ def perform_operations_on_tree(array, tree, operation, step=1000, fill_already=F
     if fill_already:
         for i in range(step, len(array) + 1, step):
             subset_array = array[:i]
-            insert_batch(tree, subset_array)
+            perform_batch_operation(tree.insert, subset_array)
 
     for i in range(step, len(array) + 1, step):
         subset_array = array[:i]
-        time = auxiliaryModule.process_algorithm_time(lambda: operation(tree, subset_array))
+        time = auxiliaryModule.process_algorithm_time(lambda: perform_batch_operation(operation, subset_array))
         time_table.append((i, time))
 
     return time_table
 
 
-def insert_batch(tree, subset_array):
+def perform_batch_operation(operation, subset_array):
     for num in subset_array:
-        tree.insert(num)
-
-
-def delete_batch(tree, subset_array):
-    for num in subset_array:
-        tree.delete(num)
-
-
-def search_batch(tree, subset_array):
-    for num in subset_array:
-        tree.search(num)
+        operation(num)
 
 
 if __name__ == "__main__":
     BST_tree = BSTTree()
     AVL_tree = AvlTree()
     list_for_trees = auxiliaryModule.generate_list()
-    table_bst_insert = perform_operations_on_tree(list_for_trees.copy(), BST_tree, insert_batch)
-    table_bst_delete = perform_operations_on_tree(list_for_trees.copy(), BST_tree, delete_batch, fill_already=True)
-    table_avl_insert = perform_operations_on_tree(list_for_trees.copy(), AVL_tree, insert_batch)
-    table_bst_search = perform_operations_on_tree(list_for_trees.copy(), BST_tree, search_batch, fill_already=True)
-    table_avl_search = perform_operations_on_tree(list_for_trees.copy(), AVL_tree, search_batch, fill_already=True)
+    table_bst_insert = perform_operations_on_tree(list_for_trees.copy(), BST_tree, BST_tree.insert)
+    table_bst_delete = perform_operations_on_tree(list_for_trees.copy(), BST_tree, BST_tree.delete, fill_already=True)
+    table_avl_insert = perform_operations_on_tree(list_for_trees.copy(), AVL_tree, AVL_tree.insert)
+    table_bst_search = perform_operations_on_tree(list_for_trees.copy(), BST_tree, BST_tree.search, fill_already=True)
+    table_avl_search = perform_operations_on_tree(list_for_trees.copy(), AVL_tree, AVL_tree.search, fill_already=True)
 
     auxiliaryModule.draw_computional_complexity([table_bst_insert, table_avl_insert], "BST and AVL trees insertion", plots=2)
     auxiliaryModule.draw_computional_complexity([table_bst_search, table_avl_search], "BST and AVL trees search", plots=2)
     auxiliaryModule.draw_computional_complexity(table_bst_delete, "BST tree deletion", plots=1)
+
+    print("Computing times for each operation:\n")
+    print("BST Insert\n")
+    print(table_avl_insert)
+    print("------------------------------------------\nAVL Insert\n")
+    print(table_avl_search)
+    print("------------------------------------------\nBST Delete\n")
+    print(table_bst_delete)
+    print("------------------------------------------\nBST Search\n")
+    print(table_bst_insert)
+    print("------------------------------------------\nAVL Search\n")
+    print(table_bst_search)
+    print("------------------------------------------\n")
+
+    new_BST = BSTTree()
+    new_AVL = AvlTree()
+    perform_batch_operation(new_BST.insert, [i for i in range(1, 21)])
+    perform_batch_operation(new_AVL.insert, [i for i in range(1, 21)])
+
+    print("Graphical representation of BST Tree: \n")
+    new_BST.print_tree()
+
+    print("\nGraphical representation of AVL Tree: \n")
+    new_AVL.print_tree()
