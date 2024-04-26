f5264bf5c45dd826ce52297f9f95d8617214d172
kgorski1 <01187228@pw.edu.pl>
2024-04-26 08:37:19 +0200

    implemented dijkstra algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -26,33 +26,12 @@ class Dijkstra:
 
         if not destinationFound or not sourceFound:
             raise WrongDataProvided("Wrong data provided")
-    
-    # K01:	S ← Ø	zbiór S ustawiamy jako pusty
-    # K02:	Q ← wszystkie wierzchołki grafu	 
-    # K03:	Utwórz n elementową tablicę d	tablica na koszty dojścia
-    # K04:	Utwórz n elementową tablicę p	tablica poprzedników na ścieżkach
-    # K05	Tablicę d wypełnij największą
-    # wartością dodatnią	 
-    # K06:	d [v] ← 0	koszt dojścia do samego siebie jest zawsze zerowy
-    # K07:	Tablicę p wypełnij wartościami -1	-1 oznacza brak poprzednika
-    # K08:	Dopóki Q zawiera wierzchołki,
-    # wykonuj kroki K09…K12	 
-    # K09:	Z Q do S przenieś wierzchołek u
-    # o najmniejszym d [u]	 
-    # K10:	Dla każdego sąsiada w wierzchołka u :
-    # wykonuj kroki K11…K12	przeglądamy sąsiadów przeniesionego wierzchołka
-    # K11:	Jeśli w nie jest w Q,
-    # to następny obieg pętli K10	szukamy sąsiadów obecnych w Q
-    # K12:	Jeśli d [w] > d [u ] + waga krawędzi u – w,
-    # to:
-    # d [w] ← d [u] + waga krawędzi u – w
-    # p [w] ← u	sprawdzamy koszt dojścia. Jeśli mamy niższy, to modyfikujemy koszt i zmieniamy poprzednika w na u
-    # K13:	Zakończ
 
-    def minimumDistance(self, Distance):
+    def minimumDistance(self, Distance, Q):
         min = float("inf")
+        minIdx = None
 
-        for v in range(self.graph.computeVertecies()):
+        for v in Q:
             if Distance[v] < min:
                 min = Distance[v]
                 minIdx = v
@@ -62,9 +41,22 @@ class Dijkstra:
     def findShortestPath(self):
         length = self.graph.computeVertecies()
 
-        S = []
         Vertecies = list(self.graph.content.keys())              # All the vertecies of the graph
         Distance = [float("inf")] * length                       # Path cost table
         Predecessors = [-1] * length                             # Predeccessor table
 
         Distance[self.source] = 0
+
+        actualVertecies = set(Vertecies)
+        while actualVertecies:
+            u = self.minimumDistance(actualVertecies)
+            actualVertecies.remove(u)
+
+            for item in self.graph.content[u]:
+                v = item[0]
+                weight = item[1]
+                if Distance[v] > Distance[u] + weight:
+                    Distance[v] = Distance[u] + weight
+                    Predecessors[v] = u
+
+        return Distance, Predecessors
