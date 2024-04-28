2eb2ed6a2ad357080c2be5aeb48a49dd5a3fbfcf
Kerciu <kerciuuu@gmail.com>
2024-04-28 13:54:44 +0200

    added board-to-graph converter

--- get_data.py
+++ get_data.py
@@ -9,3 +9,15 @@ def read_from_file(path):
         raise FileNotFoundError("File was not found")
     except Exception:
         raise Exception("Exception occured")
+
+
+def convertBoardToGraph(board: str) -> dict:
+    # ["1234", "2345", "5312"]
+
+    graph = {}
+    for i, elem in enumerate(board):
+        graph[i + 1] = []
+        for j, char in enumerate(elem):
+            if char != '0':
+                graph[i + 1].append((j + 1, char))
+    return graph
