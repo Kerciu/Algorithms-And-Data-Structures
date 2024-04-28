c76ec093148a05653bd3450c79fb6b1a33c501ed
Kerciu <kerciuuu@gmail.com>
2024-04-28 13:34:10 +0200

    added argparse and improved data collection

---
+++ app.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from get_data import read_from_file
+
+
+def parseArgs():
+    argParser = ArgumentParser()
+    argParser.add_argument("name")
+    args = argParser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parseArgs()
+    data = read_from_file(args.name)
+    print(data)
--- get_data.py
+++ get_data.py
@@ -1,7 +1,11 @@
 def read_from_file(path):
-    array_of_lines = []
-    with open(path) as file_handle:
-        for line in file_handle:
-            line = line.rstrip()
-            array_of_lines.append(line)
-    return array_of_lines
+    try:
+        with open(path) as file:
+            data = [line.strip() for line in file.readlines()]
+        return data
+    except FileExistsError:
+        raise FileExistsError("File does not exist")
+    except FileNotFoundError:
+        raise FileNotFoundError("File was not found")
+    except Exception:
+        raise Exception("Exception occured")
