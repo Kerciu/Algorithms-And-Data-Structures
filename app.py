3a1739c86cadbef39cd71a6f2ce0c92dace787ef
Kerciu <kerciuuu@gmail.com>
2024-05-01 15:11:12 +0200

    added comments

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+# -----------------------------------------------------------
+#          Find shortest path between given X points
+#              presented on a (x, y) axis logic
+#         in a text file board using Dijkstra algorithm
+# -----------------------------------------------------------
+#                   Made by Kacper Gorski
+#                 https://github.com/Kerciu
+# -----------------------------------------------------------
+#   How to use this:
+#   1. Create txt file presenting map with two X points
+#   2. Type in into cmd: python3 app.py <map.txt>
+#   3. Script will show you the shortest path
+# -----------------------------------------------------------
+
 from argparse import ArgumentParser
 from dijkstra import Dijkstra
 from graphInstance import Graph
 from programHandler import ProgramHandler
+
+# -----------------------------------------------------------
+#                       Parse Args
+# -----------------------------------------------------------
 
 
 def parseArgs():
@@ -9,6 +27,10 @@ def parseArgs():
     argParser.add_argument("name")
     args = argParser.parse_args()
     return args
+
+# -----------------------------------------------------------
+#                      Main Script
+# -----------------------------------------------------------
 
 
 if __name__ == "__main__":
@@ -24,3 +46,5 @@ if __name__ == "__main__":
     result = dijkstra.findShortestPath()
 
     print(handler.createTextPath(boardData, result))
+
+# -----------------------------------------------------------
