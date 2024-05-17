f295b5ae5f27bbb49669070547c221fed6915684
Maciej Cieslik <maciek.cieslik123@gmail.com>
2024-05-17 13:47:00 +0200

    added_tests_and_files

--- test_turingMachine.py
+++ test_turingMachine.py
@@ -1,6 +1,7 @@
 from turingMachine import turing
 
 
+# add 1 to number
 def test_turing1():
     instruction_path = "instruction1.txt"
     tape_path = "tape1.txt"
@@ -9,9 +10,27 @@ def test_turing1():
     assert correct_result == result
 
 
+# change 0 to X and 1 to Y
 def test_turing2():
     instruction_path = "instruction2.txt"
     tape_path = "tape2.txt"
     result = turing(instruction_path, tape_path)
-    correct_result = "11111B"
+    correct_result = "XYXYYX"
+    assert correct_result == result
+
+
+# delete 0 from the string
+def test_turing3():
+    instruction_path = "instruction3.txt"
+    tape_path = "tape3.txt"
+    result = turing(instruction_path, tape_path)
+    correct_result = "11111"
+    assert correct_result == result
+
+
+def test_turing4():
+    instruction_path = "instruction4.txt"
+    tape_path = "tape4.txt"
+    result = turing(instruction_path, tape_path)
+    correct_result = "1"
     assert correct_result == result
