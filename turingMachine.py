c9d98d023515d9322bff5ee2e033e661fc061e1e
kgorski1 <01187228@pw.edu.pl>
2024-05-17 15:01:22 +0200

    added comment of data structure used

--- turingMachine.py
+++ turingMachine.py
@@ -1,10 +1,22 @@
-from fileService import readFromFile
 from dataclasses import dataclass
 from transitionFunction import TransitionFunction
 
 
 @dataclass
 class Turing:
+    # transitionsDict = {
+    # 'init': {
+    #     '1': TransitionFunction(currentState='init', currentSymbol='1', newSymbol='1', direction='R', newState='init'),
+    #     '0': TransitionFunction(currentState='init', currentSymbol='0', newSymbol='0', direction='R', newState='init'),
+    #     '_': TransitionFunction(currentState='init', currentSymbol='_', newSymbol='_', direction='L', newState='carry')
+    # },
+    # 'carry': {
+    #     '1': TransitionFunction(currentState='carry', currentSymbol='1', newSymbol='0', direction='L', newState='carry'),
+    #     '0': TransitionFunction(currentState='carry', currentSymbol='0', newSymbol='1', direction='*', newState='halt'),
+    #     '_': TransitionFunction(currentState='carry', currentSymbol='_', newSymbol='1', direction='*', newState='halt')
+    # }
+    # }
+
     transitions: dict[str, dict[str, TransitionFunction]]
     tape: str
     head: int
