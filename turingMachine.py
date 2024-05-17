498e3f029ae7233cb52a1120e0de657c3649d6f7
kgorski1 <01187228@pw.edu.pl>
2024-05-17 14:58:22 +0200

    added turing class and its constructor

--- turingMachine.py
+++ turingMachine.py
@@ -1,5 +1,23 @@
 from fileService import readFromFile
+from dataclasses import dataclass
+from transitionFunction import TransitionFunction
 
-def turing(instruction_path: str, tape_path: str) -> str:  # noqa 501
-    lines, tape = readFromFile(instruction_path, tape_path)
-    return "1100"
+
+@dataclass
+class Turing:
+    transitions: dict[str, dict[str, TransitionFunction]]
+    tape: str
+    head: int
+    state: str
+
+    def __init__(self, transitions: list[TransitionFunction], tape: str) -> None:
+        self.transitions = {}
+
+        for func in transitions:
+            if func.currentState not in self.transitions:
+                self.transitions[func.currentState] = {}
+            self.transitions[func.currentState][func.currentSymbol] = func
+
+        self.tape = [*tape]
+        self.head = 0
+        self.state = "init"
