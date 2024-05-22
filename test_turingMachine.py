8eeb191106c78bf0961b8249a19b79b9cee06318
kgorski1 <01187228@pw.edu.pl>
2024-05-22 09:22:42 +0200

    added tests

--- test_turingMachine.py
+++ test_turingMachine.py
@@ -1,36 +1,98 @@
-from turingMachine import turing
+from turingMachine import Turing
+from transitionFunction import TransitionFunction
+from unittest.mock import MagicMock
+import pytest
 
 
-# add 1 to number
-def test_turing1():
-    instruction_path = "instruction1.txt"
-    tape_path = "tape1.txt"
-    result = turing(instruction_path, tape_path)
-    correct_result = "1100"
-    assert correct_result == result
+def test_initialization():
+    transitions = [
+        TransitionFunction('init', '1', '1', 'R', 'init'),
+        TransitionFunction('init', '0', '0', 'R', 'init'),
+        TransitionFunction('init', '_', '_', 'L', 'carry'),
+        TransitionFunction('carry', '1', '0', 'L', 'carry'),
+        TransitionFunction('carry', '0', '1', 'N', 'halt'),
+        TransitionFunction('carry', '_', '1', 'N', 'halt'),
+    ]
+    tape = "1011"
+    machine = Turing(transitions, tape)
+    
+    assert machine.tape == ['1', '0', '1', '1', '_']
+    assert machine.head == 0
+    assert machine.state == "init"
 
 
-# change 0 to X and 1 to Y
-def test_turing2():
-    instruction_path = "instruction2.txt"
-    tape_path = "tape2.txt"
-    result = turing(instruction_path, tape_path)
-    correct_result = "XYXYYX"
-    assert correct_result == result
+def test_take_step():
+    transitions = [
+        TransitionFunction('init', '1', '1', 'R', 'init'),
+        TransitionFunction('init', '0', '0', 'R', 'init'),
+        TransitionFunction('init', '_', '_', 'L', 'carry'),
+        TransitionFunction('carry', '1', '0', 'L', 'carry'),
+        TransitionFunction('carry', '0', '1', 'N', 'halt'),
+        TransitionFunction('carry', '_', '1', 'N', 'halt'),
+    ]
+    tape = "1011"
+    machine = Turing(transitions, tape)
+
+    assert machine.state == "init"
+    assert machine.head == 0
+    assert machine.tape == ['1', '0', '1', '1', '_']
+
+    machine.takeStep()
+    assert machine.state == "init"
+    assert machine.head == 1
+    assert machine.tape == ['1', '0', '1', '1', '_']
+
+    machine.takeStep()
+    assert machine.state == "init"
+    assert machine.head == 2
+    assert machine.tape == ['1', '0', '1', '1', '_']
 
 
-# delete 0 from the string
-def test_turing3():
-    instruction_path = "instruction3.txt"
-    tape_path = "tape3.txt"
-    result = turing(instruction_path, tape_path)
-    correct_result = "11111"
-    assert correct_result == result
+def test_run_machine():
+    transitions = [
+        TransitionFunction('init', '1', '1', 'R', 'init'),
+        TransitionFunction('init', '0', '0', 'R', 'init'),
+        TransitionFunction('init', '_', '_', 'L', 'carry'),
+        TransitionFunction('carry', '1', '0', 'L', 'carry'),
+        TransitionFunction('carry', '0', '1', 'N', 'halt'),
+        TransitionFunction('carry', '_', '1', 'N', 'halt'),
+    ]
+    tape = "1011"
+    machine = Turing(transitions, tape)
+
+    machine.runMachine()
+
+    # After running the machine, it should halt
+    assert machine.state == "halt"
+    assert machine.head == 1
+    assert machine.tape == ['1', '1', '0', '0', '_']
 
 
-def test_turing4():
-    instruction_path = "instruction4.txt"
-    tape_path = "tape4.txt"
-    result = turing(instruction_path, tape_path)
-    correct_result = "1"
-    assert correct_result == result
+@pytest.fixture
+def mocker():
+    return MagicMock()
+
+
+def test_create_machine_from_file(mocker):
+    mock_return_value = (
+        [
+            ['init', '1', '1', 'R', 'init'],
+            ['init', '0', '0', 'R', 'init'],
+            ['init', '_', '_', 'L', 'carry'],
+            ['carry', '1', '0', 'L', 'carry'],
+            ['carry', '0', '1', 'N', 'halt'],
+            ['carry', '_', '1', 'N', 'halt'],
+        ],
+        "1011"
+    )
+
+    mocker.patch('fileService.readFromFile', return_value=mock_return_value)
+
+    machine = Turing.createMachineFromFile('1011', 'instruction1.txt')
+
+    assert machine.tape == ['1', '0', '1', '1', '_']
+    assert machine.head == 0
+    assert machine.state == "init"
+    assert len(machine.transitions) == 2
+    assert 'init' in machine.transitions
+    assert 'carry' in machine.transitions
