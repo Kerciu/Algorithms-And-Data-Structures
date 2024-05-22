5bcf82e6dfb74387ab438613c2e8df057430f123
kgorski1 <01187228@pw.edu.pl>
2024-05-22 09:11:22 +0200

    added normal sequence of arguments to create machine

--- mainApp.py
+++ mainApp.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         raise SyntaxError("python3 <tape> <instructions file>")
 
-    turing = Turing.createMachineFromFile(sys.argv[2], sys.argv[1])
+    turing = Turing.createMachineFromFile(sys.argv[1], sys.argv[2])
     turing.runMachine()
